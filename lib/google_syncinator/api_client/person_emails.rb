6dab8e8710c7f5bcfc1a2a98a77df62eb9d5644a
Devin Hanlon <devin.hanlon@biola.edu>
2016-08-03 19:15:06 -0700

    Updated alterable person email fields

--- lib/google_syncinator/api_client/person_emails.rb
+++ lib/google_syncinator/api_client/person_emails.rb
@@ -10,7 +10,7 @@ module GoogleSyncinator
       end
 
       put :update, '/person_emails/{id}' do |resource|
-        resource.required :address
+        resource.optional :address, :first_name, :vfe, :password, :uuid
       end
     end
   end
