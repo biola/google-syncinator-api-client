3c0eb5949d4cb879be55fd44bc786e6ca3bfff61
Devin Hanlon <devin.hanlon@biola.edu>
2017-05-02 12:32:46 -0700

    added primary to person email optional attrs.

--- lib/google_syncinator/api_client/person_emails.rb
+++ lib/google_syncinator/api_client/person_emails.rb
@@ -7,10 +7,12 @@ module GoogleSyncinator
 
       post :create, '/person_emails' do |resource|
         resource.required :uuid, :address
+        resource.optional :primary
       end
 
       put :update, '/person_emails/{id}' do |resource|
-        resource.optional :address, :uuid, :first_name, :last_name, :vfe, :password, :privacy
+        resource.optional :address, :uuid, :first_name, :last_name, :vfe,
+                          :password, :privacy
       end
     end
   end
